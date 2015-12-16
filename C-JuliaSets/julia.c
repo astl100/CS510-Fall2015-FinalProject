03f5a22c16b2d167c478fe026b943f7364ebdd4a
Lindy Astl <astl100@mail.chapman.edu>
2015-12-16 07:59:56 -0800

    C files creating complex plane

--- C-JuliaSets/julia.c
+++ C-JuliaSets/julia.c
@@ -1,20 +1,23 @@
+#include <stdio.h>
+//cplane.h implicitly includes complex.h
 #include "cplane.h"
+
 
 int main(int argc, char **argv)
 {
 	//error message in case not 8 inputs
 	if (argc !=9)
-	{printf("Incorrect amount of inputs");}
+	{printf("Incorrect amount of inputs");
+	return 1;}
 
 	//PARSE INPUTS TO BE LONG DOUBLES;
 	long double xmin, xmax, ymin, ymax, xpoints, ypoints;
-	sscanf(argv[0], "%Lg", &xmin);
-	sscanf(argv[1], "%Lg", &xmax);
-	sscanf(argv[2], "%Lg", &ymin);
-	sscanf(argv[3], "%Lg", &ymax);
-	sscanf(argv[4], "%Lg", &xpoints);
-	sscanf(argv[5], "%Lg", &ypoints);
-
+	sscanf(argv[1], "%Lg", &xmin);
+	sscanf(argv[2], "%Lg", &xmax);
+	sscanf(argv[3], "%Lg", &ymin);
+	sscanf(argv[4], "%Lg", &ymax);
+	sscanf(argv[5], "%Lg", &xpoints);
+	sscanf(argv[6], "%Lg", &ypoints);
 
 	long double creal, cimag;
 	sscanf(argv[7], "%Lg", &creal); 
@@ -25,12 +28,13 @@ int main(int argc, char **argv)
  	* 	another command line argumant is the imaginary portion
  	* create a complex plane
  	* 	use first 6 command line aruments as parameters for the creation of the plane*/	
-	COMPLEX c = complex_setting(creal, cimag);
-	CPLANE cp = cplane_setting(xmin, xmax, ymin, ymax, xpoints, ypoints);
+	COMPLEX *c = complex_setting(creal, cimag);
+	CPLANE *cp = cplane_setting(xmin, xmax, ymin, ymax, xpoints, ypoints);
 
 	/*outputs the amount of iterations take to:
  	*	reach a magnitude greater than 2
  	*	OR reach the max amount of iterations
- 	*this is done for every coordinate on the complex plane*/	 	cplane_iterate(cp,c);
+ 	*this is done for every coordinate on the complex plane*/	 	
+	cplane_iterate(&cp,&c);
 }
    
